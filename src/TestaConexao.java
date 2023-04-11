ee539b2c095c8d3dfd8431a4f277b53f9c0fde63
Miltonnnotforyou <miltonnotforyou@yahoo.com.br>
2023-04-11 08:45:44 -0300

    renomeando para Main

--- src/TestaConexao.java
+++ src/TestaConexao.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class TestaConexao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager.
+                getConnection("jdbc:mysql://localhost/loja_virtual?Timezone=true&serverTimezone=UTC", "root", "20Emanu02");
+
+        System.out.println("Fechando a conexão");
+
+        connection.close();
+    }
 }
