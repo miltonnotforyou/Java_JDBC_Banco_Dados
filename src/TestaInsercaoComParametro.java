605a1c7be4a64dd73e851aa12cb20b8f476d7b98
Miltonnnotforyou <miltonnotforyou@yahoo.com.br>
2023-04-13 08:57:36 -0300

    adicionando arquivos do curso

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -8,19 +8,39 @@ public class TestaInsercaoComParametro {
         String descricao = "Mouse sem fio";
 
         ConnectionFactory connectionFactory = new ConnectionFactory();
-        Connection connection = connectionFactory.recuperarConexao();
+        try (Connection connection = connectionFactory.recuperarConexao()) {
 
-        PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (NOME, descricao) VALUES(?, ?)", Statement.RETURN_GENERATED_KEYS );
+            connection.setAutoCommit(false);
 
+            try (PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (NOME, descricao) VALUES(?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+                adicionaVariavel("SmartTV", "45 polegadas", stm);
+                adicionaVariavel("Radio", "bateria", stm);
+
+                connection.commit();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Rollback executado com sucesso!");
+                connection.rollback();
+            }
+        }
+    }
+
+    private static void adicionaVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
         stm.setString(1, nome);
         stm.setString(2, descricao);
 
+        if (nome.equals("Radio")) {
+            throw new RuntimeException("Não foi possivel adicionar o produto");
+        }
+
         stm.execute();
 
-        ResultSet rst = stm.getGeneratedKeys();
-        while (rst.next()) {
-            Integer id = rst.getInt(1);
-            System.out.println("O ID criado foi " + id);
+        try (ResultSet rst = stm.getGeneratedKeys()) {
+            while (rst.next()) {
+                Integer id = rst.getInt(1);
+                System.out.println("O ID criado foi " + id);
+            }
         }
     }
 }
